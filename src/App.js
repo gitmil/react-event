612189416d3e6b5baa0fb04e00476f7ead030658
techsithgit <hemilp@tuplecorp.com>
2019-05-29 22:00:17 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
+const initState = {
+  name: "",
+  age: null
+};
 function App() {
+  const [obj, setObj] = useState(initState);
+
+  const nameChange = e => {
+    const copyObj = { ...obj };
+    copyObj.name = e.target.value;
+    setObj(copyObj);
+  };
+
+  const setValue = () => {
+    const copyObj = { ...obj };
+    copyObj.name = "default";
+    setObj(copyObj);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" onChange={nameChange} value={obj.name} />
+        <span>{obj.name}</span>
+        <button onClick={setValue}>Set</button>
       </header>
     </div>
   );
